0eda628f26a16299cace9e79af5000eb65939565
Andrés Felipe Vargas V <afvasquez@uninorte.edu.co>
2024-03-15 23:16:30 +0000

    taller finalizado

--- taller-03.js
+++ taller-03.js
@@ -16,6 +16,20 @@ function desglosarString(str, opcion) {
 }
 
 
-const cadena = "Hola Mundo";
-console.log("Cantidad de vocales:", desglosarString(cadena, "vocales")); 
-console.log("Cantidad de consonantes:", desglosarString(cadena, "consonantes"));
+// Punto 2
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const num1 = nums[i];
+        
+        for (let j = i + 1; j < nums.length; j++) {
+            const num2 = nums[j];
+            const suma = num1 + num2;
+            
+            if (suma === target) {
+                return [i, j];
+            }
+        }
+    }
+    
+    return [];
+}
