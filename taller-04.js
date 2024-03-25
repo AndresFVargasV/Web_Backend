e0d325a4ab1b91e0ed264be9e4703eb79b683169
AndresFVargasV <afvasquez@uninorte.edu.co>
2024-03-25 18:21:18 -0500

    Taller 04 finished

--- taller-04.js
+++ taller-04.js
@@ -1,31 +1,41 @@
 // Punto 1
-function promedioFinal(listaNumeros){
-    return listaNumeros.flat().reduce((acumulado, valor) => acumulado + +valor, 0)/listaNumeros.flat().length
+function promedioFinal(listaNumeros) {
+  return listaNumeros.flat().reduce((acumulado, valor) => acumulado + +valor, 0) /
+    listaNumeros.flat().length;
 }
-
 
 // Punto 2
-function limpiarNombre(nombre){
-    return nombre.trim().split(" ")
+function limpiarNombre(nombre) {
+  return nombre.trim().split(" ");
 }
 
-
 // Punto 3
-function sortObjects(listaObjetos, key){
-    return listaObjetos.sort((item1, item2) => item1[key] - item2[key])
-}   
+function sortObjects(listaObjetos, key) {
+  return listaObjetos.sort((item1, item2) => item1[key] - item2[key]);
+}
 
 // Punto 4
-function limpiarLista(user){
-    const [name, curso, ...notas] = user.split(",")
+function limpiarLista(data_users) {
+  const users = data_users.split(";").slice(1,-1)
+  
+  const lista_limpia = users.map(user => {
+    const [name, curso, ...notas] = user.split(",");
 
-    return [{nombre: limpiarNombre(name)[0], 
-    apellido: limpiarNombre(name)[1], 
-    notas: notas.slice(0,-1), 
-    final: promedioFinal(notas.slice(0,-1))}]
+    return {
+        nombre: limpiarNombre(name)[0],
+        apellido: limpiarNombre(name)[1],
+        curso: curso,
+        notas: notas.slice(0, -1),
+        final: promedioFinal(notas.slice(0, -1)),
+      };
+  })
+
+  return lista_limpia
 }
 
 // Punto 5
-function bestlnClass(){
-    
+function bestlnClass(data_csv, curso){
+  estudiantes = limpiarLista(data_csv)
+  return sortObjects(estudiantes, "final").filter(estudiante => estudiante.curso === curso).slice(-3);
 }
+
